3f7b95931315fcf0a4505fb1b064933c96db7630
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-11-10 18:29:00 -0500

    Updated Scripts

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -11,10 +11,13 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
+    public TextAsset            layoutXML;
 
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
+    public Layout               layout;
+    public List<CardProspector> drawPile;
 
 	void Awake(){
 		S = this; //Set up a Singleton for Prospector
@@ -24,12 +27,30 @@ public class Prospector : MonoBehaviour {
 		deck = GetComponent<Deck> (); //Get the Deck
 		deck.InitDeck (deckXML.text); //Pass DeckXML to it
         Deck.Shuffle(ref deck.cards);
-        Card c;
+
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    /*  Card c;
         for (int cNum=0; cNum<deck.cards.Count; cNum++)
         {
             c = deck.cards[cNum];
             c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
         }
+     */
 	}
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card>lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach(Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
+
 
 }
