bfb3eb75fb3c959dd165923db217ee0060c56349
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-11-10 18:09:06 -0500

    Layout Script

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -23,6 +23,13 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> (); //Get the Deck
 		deck.InitDeck (deckXML.text); //Pass DeckXML to it
+        Deck.Shuffle(ref deck.cards);
+        Card c;
+        for (int cNum=0; cNum<deck.cards.Count; cNum++)
+        {
+            c = deck.cards[cNum];
+            c.transform.localPosition = new Vector3((cNum % 13) * 3, cNum / 13 * 4, 0);
+        }
 	}
 
 }
