9eb544643410bea12fa7c30eed7874bb87b49939
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-11-10 19:42:57 -0500

    Update Scripts

--- Assets/__Scripts/CardProspector.cs
+++ Assets/__Scripts/CardProspector.cs
@@ -17,4 +17,10 @@ public class CardProspector : Card
     public List<CardProspector> hiddenBy = new List<CardProspector>();
     public int layoutID;
     public SlotDef slotDef;
+
+    public override void OnMouseUpAsButton()
+    {
+        Prospector.S.CardClicked(this);
+        base.OnMouseUpAsButton();
+    }
 }
