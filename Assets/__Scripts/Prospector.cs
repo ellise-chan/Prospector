3e87360b1e005344c952e8ea66176008fbbd6fc9
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-11-10 21:00:23 -0500

    Update _Scripts

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -170,6 +170,7 @@ public class Prospector : MonoBehaviour {
                 MoveToDiscard(target);
                 MoveToTarget(Draw());
                 UpdateDrawPile();
+                ScoreManager.EVENT(eScoreEvent.draw);
                 break;
             case eCardState.tableau:
                 bool validMatch = true;
@@ -185,6 +186,7 @@ public class Prospector : MonoBehaviour {
                 tableau.Remove(cd);
                 MoveToTarget(cd);
                 SetTableauFaces();
+                ScoreManager.EVENT(eScoreEvent.mine);
                 break;
         }
         CheckForGameOver();
@@ -214,11 +216,13 @@ public class Prospector : MonoBehaviour {
     {
         if (won)
         {
-            print("Game Over. You won! :)");
+            //print("Game Over. You won! :)");
+            ScoreManager.EVENT(eScoreEvent.gameWin);
         }
         else
         {
-            print("Game Over. You Lost. :(");
+            //print("Game Over. You Lost. :(");
+            ScoreManager.EVENT(eScoreEvent.gameLoss);
         }
         SceneManager.LoadScene("__Prospector_Scene_0");
     }
